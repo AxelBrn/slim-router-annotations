219aaa8c3dc0727d7242aa27e865119cf87bdd23
Axel Brionne <axel.brionne@gmail.com>
2021-11-16 22:45:35 +0100

    feat: add entry point of the package

---
+++ src/SlimRouterAnnotations.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RouterAnnotations;
+
+use ReflectionClass;
+use ReflectionException;
+use RouterAnnotations\Utils\ControllerClass;
+use Slim\App;
+
+class SlimRouterAnnotations
+{
+
+    /**
+     * @throws ReflectionException
+     */
+    public static function read(App $app, string $scanDir): void
+    {
+        $files = scandir('../'.$scanDir.DIRECTORY_SEPARATOR);
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                if (str_contains($file, '.php')) {
+                    $cwd = getcwd();
+                    if (!$cwd) {
+                        continue;
+                    }
+                    $parent =  dirname($cwd);
+                    require_once($parent.DIRECTORY_SEPARATOR.$scanDir.DIRECTORY_SEPARATOR.$file);
+                    $arrayClass = get_declared_classes();
+                    $className = end($arrayClass);
+                    if (!$className) {
+                        continue;
+                    }
+                    $reflectionClass = new ReflectionClass($className);
+
+                    $controllerClass = new ControllerClass($reflectionClass);
+                    $controllerClass->generateRouting($app);
+                }
+            }
+        }
+    }
+
+}
